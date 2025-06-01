c66a19e3fb4c63df2b89dae46014b034ebf61225
akash777js <josimjs786@gmail.com>
2025-06-01 13:15:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ keyboard = InlineKeyboardMarkup(
 
 # Image URLs for the random image feature
 image_urls = [
-    "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg",
-    "https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png",
+    "",
+    "",
     # Add more image URLs as needed
 ]
 
